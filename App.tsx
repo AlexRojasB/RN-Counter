aa28252c68a617b46a80d1438448e2e4e7e0f33d
Alexander Rojas Benavides <alexrrojas.b@gmail.com>
2022-03-05 23:24:46 -0600

    flex screen

--- App.tsx
+++ App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import {SafeAreaView, SafeAreaViewBase, Text, View} from 'react-native';
 import {BoxObjectModel} from './src/screens/BoxObjectModel/boxObjectModel.screen';
 import {CountdownScreen} from './src/screens/CountDownScreen/countdown.screen';
-import { Dimensiones } from './src/screens/Dimensiones/dimensiones.screen';
-import { PositionScreen } from './src/screens/Position/position.screen';
+import {Dimensiones} from './src/screens/Dimensiones/dimensiones.screen';
+import {FlexScreen} from './src/screens/Flex/flex.screen';
+import {PositionScreen} from './src/screens/Position/position.screen';
 
 const App = () => {
   return (
     // return <CountdownScreen />;
     <SafeAreaView style={{flex: 1}}>
-      <PositionScreen />
+      <FlexScreen />
     </SafeAreaView>
   );
 };
